d38e9d290b44cd9b420ec6f2dd31c8dc6ef88874
Victor Holanda <jvictorweb21@gmail.com>
2020-03-23 16:25:05 -0300

    Adicionado estoque_saida_list

--- projeto/estoque/urls.py
+++ projeto/estoque/urls.py
@@ -4,9 +4,11 @@ from projeto.estoque import views as v
 app_name = "estoque"
 
 urlpatterns = [
-	path('', v.estoque_entrada_list, name="estoque_entrada_list"),
-	path('<int:pk>/', v.estoque_entrada_detail, name="estoque_entrada_detail"),
+	path('entrada/', v.estoque_entrada_list, name="estoque_entrada_list"),
+	path('entrada/<int:pk>/', v.estoque_entrada_detail, name="estoque_entrada_detail"),
+	path('entrada/add/', v.estoque_entrada_add, name="estoque_entrada_add"),
 	# path('<int:pk>/edit/', v.EstoqueUpdate.as_view(), name="estoque_update"),
 	# path('add/', v.EstoqueCreate.as_view(), name="estoque_add"),
-	path('add/', v.estoque_entrada_add, name="estoque_entrada_add"),
+
+	path('saida/', v.estoque_saida_list, name="estoque_saida_list"),
 ]
